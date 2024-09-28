a72714f06dd4e075cebdb46961d4e8cc9748b230
Vera <v.bergstrom83@yahoo.se>
2024-09-28 12:57:13 +0200

    add page services and services data

---
+++ lib/data.ts
@@ -0,0 +1,25 @@
+//maybe a booking button????
+//do I need to have a bool for the card to be flipped or not?
+//add all text data
+
+export const servicesData = [
+  {
+    title: "WE REPLANT",
+    image: "New York, NY",
+
+    description:
+      "Plants need room to grow, we will make sure your plants get new pots, new soil and make sure they get the best chance to blossom.",
+  },
+  {
+    title: "WE REVIVE",
+    image: "Orlando, FL",
+    description:
+      "bugs, diseases, and other issues can harm your plants. We will make sure to identify, target and treat your plants back to health.",
+  },
+  {
+    title: "WE ADVICE",
+    image: "Houston, TX",
+    description:
+      "You can chat with us about your plants, you can also ask our smart plant assistant for help at any hours of the day for SOS advice.",
+  },
+] as const;
